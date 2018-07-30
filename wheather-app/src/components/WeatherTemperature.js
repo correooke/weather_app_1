66ae8182748ffa5332c2146e8b72dcde40b29a0e
HSNBDES33\eocariz <eocariz@hasar.com>
2018-07-30 20:45:20 -0300

    Prop-Types

--- wheather-app/src/components/WeatherTemperature.js
+++ wheather-app/src/components/WeatherTemperature.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
+import PropTypes from 'prop-types';
 import {
     CLOUD,
     CLOUDY,
@@ -35,5 +36,10 @@ const WeatherTemperature = ({ temperature, weatherState }) => (
         <span>{ `${temperature} C°`}</span>
     </div>
 );
+
+WeatherTemperature.propTypes = {
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired,
+};
 
 export default WeatherTemperature;
