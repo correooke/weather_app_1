5a0701fa82b7ba374d40da4354f84333d61672a3
HSNBDES33\eocariz <eocariz@hasar.com>
2018-07-30 21:16:27 -0300

    Validaciones con PropTypes (más validaciones)

--- wheather-app/src/components/Location.js
+++ wheather-app/src/components/Location.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Location = ({ city }) => (
         <div>
@@ -7,5 +8,9 @@ const Location = ({ city }) => (
             </h1>
         </div>
     );
+
+Location.propTypes = {
+    city: PropTypes.string.isRequired,
+};
 
 export default Location;
--- wheather-app/src/components/WeatherExtraInfo.js
+++ wheather-app/src/components/WeatherExtraInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const WeatherExtraInfo = ({ humidity, wind }) => (
     <div>
@@ -6,5 +7,10 @@ const WeatherExtraInfo = ({ humidity, wind }) => (
         <span>{`${wind} viento`}</span>
     </div>
 );
+
+WeatherExtraInfo.propTypes = {
+    humidity: PropTypes.number.isRequired,
+    wind: PropTypes.string.isRequired,
+};
 
 export default WeatherExtraInfo;
