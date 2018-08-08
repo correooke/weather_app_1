6f7cffece2fdfb1e13d76b25f34f09a398f0abe1
HSNBDES33\eocariz <eocariz@hasar.com>
2018-08-08 13:00:36 -0300

    Estilos componente WeatherExtraInfo

--- wheather-app/src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
+++ wheather-app/src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
@@ -4,8 +4,8 @@ import './styles.css';
 
 const WeatherExtraInfo = ({ humidity, wind }) => (
     <div className="weatherExtraInfoCont">
-        <span>{`${humidity} % - `}</span>
-        <span>{`${wind} viento`}</span>
+        <span className="extraInfoText">{`Humedad: ${humidity} % - `}</span>
+        <span className="extraInfoText">{`Vientos: ${wind}`}</span>
     </div>
 );
 
